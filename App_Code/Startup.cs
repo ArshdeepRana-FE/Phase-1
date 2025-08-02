7d40e1551165a61a4032bf835ce223330575e528
ArshdeepRana-FE <arshdeep.rana@joshtechnologygroup.com>
2025-08-02 14:37:13 +0530

    AR_RU_02 : Authentication and Authorization : Rebased all changes

--- App_Code/Startup.cs
+++ App_Code/Startup.cs
@@ -1,20 +1,12 @@
 ﻿namespace RestaurantManagementApplication
 {
     /// <summary>
-<<<<<<< HEAD
-    /// Defines the startup configuration for the application.
-=======
     /// Represents the startup configuration class for the application.
->>>>>>> fd9161b (AR_RU_02 : Authentication and Authorization : Rebased all changes)
     /// </summary>
     public partial class Startup
     {
         /// <summary>
-<<<<<<< HEAD
-        /// Contains logic to be executed during application startup.
-=======
         /// Configures the application at startup.
->>>>>>> fd9161b (AR_RU_02 : Authentication and Authorization : Rebased all changes)
         /// </summary>
         public void Configuration()
         {
--- Default.aspx
+++ Default.aspx
@@ -1,5 +1,1 @@
 ﻿<%@ Page Title="Home Page" Language="C#"  AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
-<<<<<<< HEAD
-=======
-
->>>>>>> fd9161b (AR_RU_02 : Authentication and Authorization : Rebased all changes)
